3bd8b1228369bac8ebc19379ba1e84367f66c539
Gigi-G <seminara.luigi@gmail.com>
2022-08-05 16:07:06 +0200

    feat: camera

---
+++ lib/camera/camera.cpp
@@ -0,0 +1,103 @@
+#include <camera.h>
+
+void Camera::init() {
+    Serial.printf("\nInit camera...");
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_GRAYSCALE;
+    
+    if(psramFound()){
+        config.frame_size = FRAMESIZE_96X96;
+        config.jpeg_quality = 10;
+        config.fb_count = 1;
+    } else {
+        config.frame_size = FRAMESIZE_SVGA;
+        config.jpeg_quality = 12;
+        config.fb_count = 1;
+    }
+    
+    // Camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        exit(-1);
+    }
+
+    sensor_t * s = esp_camera_sensor_get();
+    //s->set_framesize(s, FRAMESIZE_96X96);
+    //s->set_gain_ctrl(s, 1);
+    //s->set_exposure_ctrl(s, 1);
+    //s->set_awb_gain(s, 1);                        
+    //s->set_special_effect(s, 2);
+    //s->set_pixformat(s, PIXFORMAT_GRAYSCALE);
+
+    Serial.printf("OK\n");
+}
+
+esp_err_t Camera::getImage(QueueImage<uint8_t*>* q) {
+    esp_err_t res = ESP_OK;
+    _jpg_buf_len = 0;
+    _jpg_buf = NULL;
+
+    fb = esp_camera_fb_get();
+    if (!fb) {
+        Serial.println("Camera capture failed");
+        res = ESP_FAIL;
+    }
+    else if(fb->width > 32) {
+        uint8_t* frame = new uint8_t[fb->len];
+        memcpy(frame, fb->buf, fb->len);
+        q->push(frame);
+        if(fb->format != PIXFORMAT_JPEG){
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+            esp_camera_fb_return(fb);
+            fb = NULL;
+            if(!jpeg_converted){
+                Serial.println("JPEG compression failed");
+                res = ESP_FAIL;
+            }
+        }
+        else {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
+        }
+    }
+    return res;
+}
+
+void Camera::restore() {
+    if(fb){
+        esp_camera_fb_return(fb);
+        fb = NULL;
+        _jpg_buf = NULL;
+    } else if(_jpg_buf){
+        free(_jpg_buf);
+        _jpg_buf = NULL;
+    }
+}
+
+size_t Camera::getJPGBufLen() {
+    return this->_jpg_buf_len;
+}
+
+uint8_t* Camera::getJPGBuf() {
+    return this->_jpg_buf;
+}
---
+++ lib/camera/camera.h
@@ -0,0 +1,40 @@
+#ifndef CAMERA
+#define CAMERA
+
+#include <Arduino.h>
+#include <esp_camera.h>
+#include <queueImage.h>
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+class Camera {
+    private:
+        camera_fb_t* fb;
+        size_t _jpg_buf_len;
+        uint8_t* _jpg_buf;
+
+    public:
+        void init();
+        esp_err_t getImage(QueueImage<uint8_t*>* q);
+        void restore();
+        size_t getJPGBufLen();
+        uint8_t* getJPGBuf();
+};
+
+#endif
